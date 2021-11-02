f25d34dfec6cf0a0952cc1e73e84b002dfa6633a
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-11-02 14:10:33 +0300

    Removed sources

--- app/request.py
+++ app/request.py
@@ -1,6 +1,6 @@
 from app import app
 import urllib.request,json
-from .models import News,Sources
+from .models import News
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
 # Getting the movie base url
