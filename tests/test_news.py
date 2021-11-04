90b9b2bb6f3fe8ae5af995ef4271edf558b0d84a
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-11-04 05:09:09 +0300

    updated the test file

--- tests/test_news.py
+++ tests/test_news.py
@@ -1,18 +1,18 @@
 import unittest
-from app.models import Movie, News
+from app.models import News
 
 class MovieTest(unittest.TestCase):
     '''
-    Test Class to test the behaviour of the Movie class
+    Test Class to test the behaviour of the News class
     '''
 
     def setUp(self):
         '''
         Set up method that will run before every Test
         '''
-        self.new_news = News(1234,'POLITICAL NEWS','US PRESIDENCY','/khsjha27hbs','08/10/2021')
+        self.new_news = News(111245,'POLITICAL NEWS','US PRESIDENCY','/khsjha27hbs','08/10/2021')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
 
-        # urlToImage, title,description,url, publishedAt
+        
